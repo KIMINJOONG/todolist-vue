16bc89215b4cec448c1a3e1924649f4f53ee5351
KIMINJOONG <ijboym216@gmail.com>
2019-04-23 22:08:18 +0900

    Let's make todolist with vue part two

---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-card class="pa-3" v-for="(list, index) in todoList" :key="index">
+      <p>{{list.memo}}</p>
+      <v-btn fab flat small color="green">
+        <i class="fas fa-check"></i>
+      </v-btn>
+      <v-btn fab flat small color="blue">
+        <i class="fas fa-redo-alt"></i>
+      </v-btn>
+      <v-btn fab flat small color="red">
+        <i class="fas fa-trash-alt"></i>
+      </v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todoList"]
+};
+</script>
