13256656b99f80650f80ed50be6027ce177e6730
KIMINJOONG <ijboym216@gmail.com>
2019-04-27 10:42:15 +0900

    add function delete, complete, redo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs6 pa-2>
-        <List :todoList="todoList"/>
+        <List :todoList="todoList" @statusControl="statusControl" @listDelete="listDelete"/>
       </v-flex>
       <v-flex xs6 pa-2>
         <ListAdd @listAdd="listAdd"/>
@@ -29,6 +29,12 @@ export default {
     listAdd(memo) {
       console.log("받았어");
       this.todoList.push({ memo: memo, status: "created" });
+    },
+    statusControl(index, status) {
+      this.todoList[index].status = status;
+    },
+    listDelete(index) {
+      this.todoList.splice(index, 1);
     }
   }
 };
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <v-card class="pa-3" v-for="(list, index) in todoList" :key="index">
+    <v-card
+      class="pa-3"
+      :class="{'done': list.status === 'done'}"
+      v-for="(list, index) in todoList"
+      :key="index"
+    >
       <p>{{list.memo}}</p>
-      <v-btn fab flat small color="green">
+      <v-btn
+        v-if="list.status === 'created'"
+        @click="$emit('statusControl', index, 'done')"
+        fab
+        flat
+        small
+        color="green"
+      >
         <i class="fas fa-check"></i>
       </v-btn>
-      <v-btn fab flat small color="blue">
+      <v-btn v-else @click="$emit('statusControl', index, 'created')" fab flat small color="blue">
         <i class="fas fa-redo-alt"></i>
       </v-btn>
-      <v-btn fab flat small color="red">
+      <v-btn @click="$emit('listDelete', index)" fab flat small color="red">
         <i class="fas fa-trash-alt"></i>
       </v-btn>
     </v-card>
@@ -20,3 +32,12 @@ export default {
   props: ["todoList"]
 };
 </script>
+<style scoped>
+.done {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.done p {
+  text-decoration: line-through;
+  color: rgba(0, 0, 0, 0.5);
+}
+</style>
