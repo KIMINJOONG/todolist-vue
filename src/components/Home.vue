fe82cf46a670913488d58642f7320a8c37619264
KIMINJOONG <ijboym216@gmail.com>
2019-04-28 09:08:10 +0900

    finish

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <v-container>
     <v-layout row wrap>
+      <v-flex xs12 text-xs-center>
+        <h1>투두 리스트</h1>
+        <p>전체 할 일: {{todoList.length}} / 완료 된 할 일 : {{countDone}} / 남은 할일: {{todoList.length - countDone}}</p>
+      </v-flex>
       <v-flex xs6 pa-2>
         <List :todoList="todoList" @statusControl="statusControl" @listDelete="listDelete"/>
       </v-flex>
       <v-flex xs6 pa-2>
-        <ListAdd @listAdd="listAdd"/>
+        <ListAdd @listAdd="listAdd" @listEdit="listEdit"/>
       </v-flex>
     </v-layout>
   </v-container>
@@ -25,9 +29,17 @@ export default {
       todoList: []
     };
   },
+  computed: {
+    countDone() {
+      let count = 0;
+      this.todoList.forEach(list => {
+        if (list.status === "done") count++;
+      });
+      return count;
+    }
+  },
   methods: {
     listAdd(memo) {
-      console.log("받았어");
       this.todoList.push({ memo: memo, status: "created" });
     },
     statusControl(index, status) {
@@ -35,6 +47,9 @@ export default {
     },
     listDelete(index) {
       this.todoList.splice(index, 1);
+    },
+    listEdit(memo, index) {
+      this.todoList[index].memo = memo;
     }
   }
 };
